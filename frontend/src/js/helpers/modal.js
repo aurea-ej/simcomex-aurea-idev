c374f01f69e8ac0da08156046f3e9e8e50c855c3
HeitorRamos132 <heitorgpramos95@gmail.com>
2021-05-23 14:50:41 -0300

    Modal atualizado

--- frontend/src/js/helpers/modal.js
+++ frontend/src/js/helpers/modal.js
@@ -8,6 +8,11 @@ function modalToggle(){
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none'
     })
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
 }
 
 export default modalToggle
